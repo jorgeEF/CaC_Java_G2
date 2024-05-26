c9e541cb46e500d9a2b09750ccd2f6823b6b879f
Mldamico <matias.lioneldamico@gmail.com>
2024-05-26 17:32:32 -0300

    Added api

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 const content = document.getElementById("main-content");
+const jokeBtn = document.getElementById("joke-btn");
+const jokeText = document.getElementById("joke-text");
+
+jokeBtn.addEventListener("click", getJoke);
+
+function getJoke() {
+  fetch("https://v2.jokeapi.dev/joke/Any?lang=es&type=single")
+    .then((response) => response.json())
+    .then((data) => (jokeText.innerText = data.joke));
+}
 
 function toggleMobileMenu(menu) {
   menu.classList.toggle("open");
